e50d2c72005711f3534012edd8963ff6f33af0ea
Xiaozhou Jia <jiaxiaozhou@gmail.com>
2018-05-31 16:05:23 +0900

    Change model to add server name in the model

--- src/main/kotlin/xzjia/kuboolin/model/Model.kt
+++ src/main/kotlin/xzjia/kuboolin/model/Model.kt
@@ -1,8 +1,9 @@
 package xzjia.kuboolin.model
 
+import java.net.InetAddress
 import java.time.LocalDateTime
 
-data class CounterState(val value: Long, val asOf: LocalDateTime = LocalDateTime.now())
+data class CounterState(val value: Long, val asOf: LocalDateTime = LocalDateTime.now(), val by: String = InetAddress.getLocalHost().hostName)
 
 sealed class CounterEvent(val type: String, val at: LocalDateTime = LocalDateTime.now())
 
